c20a4eaf3344ae5719255ed7021f0284bfa789f9
Nikhil Dilip Kulkarni <nikhil11kulkarni@gmail.com>
2018-09-12 18:32:06 +0530

    main uodated

--- main.cpp
+++ main.cpp
@@ -5,9 +5,23 @@
 #include "Board.h"
 using namespace std;
 
-// const int depot=0;
-// const int startCustomer=1,endCustomer=1000;
-// const int startCharging=1001,endCharging=1100;
+
+template<typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        *(result++) = item;
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
+
+
 
 int main() {
 Board board;
@@ -15,24 +29,39 @@ Board board;
 while(true){
 	string opponentMove;
 	std::getline (std::cin,opponentMove);
-    std::istringstream buf(opponentMove);
-    std::istream_iterator<std::string> beg(buf), end;
-    std::vector<std::string> tokens(beg, end); // done!
-		    //cout<<tokens[0]<<"amaa";
-		    //for(auto& s: tokens)std::cout << s << endl; //PRINTS INPUT
-//tokens is a vector conating input
-    if(tokens[0]=="P"){
-    	int hexagon = stoi(tokens[1]);
-    	int position = stoi(tokens[2]);
-    	board.setRing(0,hexagon,position);
+    std::vector<std::string> tokens = split(opponentMove, ' '); //tokens is a vector conating input//for(auto& s: tokens)std::cout << s << endl;
+    //Tokens can be-- P 1 2 | S 1 2 M 2 4 | RS 1 2 RE 4 16 X 3 4 | S 1 2 M 2 4 RS 1 2 RE 4 16 X 3 4
+    int countTokens=tokens.size();
+    int readTokens=0;
+while(countTokens>0){
+
+    if(tokens[0]=="P"){ //place a ring
+        readTokens++;
+        int hexagon = stoi(tokens[readTokens]);
+        readTokens++;
+        int position = stoi(tokens[readTokens]);
+        board.setRing(0,hexagon,position);
+        
     }
-    cout<<tokens[0]<<" ring set";
+    else if(tokens[0]=="S"){
+        if(tokens.size()==)
+        int hexagon = stoi(tokens[1]);
+        int position = stoi(tokens[2]);
+        board.setRing(0,hexagon,position);
+    }
+    else if(tokens[0]=="RS"){ //remove row of markers and ring
+        int hexagon = stoi(tokens[1]);
+        int position = stoi(tokens[2]);
+        board.setRing(0,hexagon,position);
+    }    
+    countTokens--;
+}
 
+    
+
+    //OUR Turn... Move wisely! Best luck!
 }
 
 }
-
-
- 
 
  
