a49e1883a657669da46e97a67c584fbcb9fcf1d0
Pushpam-iitd <Pushpam.Anand.cs116@cse.iitd.ac.in>
2018-09-13 15:07:35 +0530

    laal hai laal rhega

--- Board.h
+++ Board.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 #ifndef BOARD_H
@@ -18,9 +17,17 @@ class Point{
 	void sety(int y1);
 } ;
 
-class Board{
 
-public:
+Point twod_from_hex (Point p);
+	
+Point hex_from_twod (Point p);
+
+class Board{
+	public:
+	int boardsize = 11;
+	int boardArray [11] [11];
+    bool validArray [11] [11];
+
 	/**
      * Constructor : Creates the empty board with default size corresponding to 5 rings
      * 
@@ -79,86 +86,3 @@ public:
 #endif	/* BOARD_H */
 
 
-=======
-
-
-#ifndef BOARD_H
-#define	BOARD_H
-
-#include<iostream>
-#include<fstream>
-using namespace std;
-
-class Point{
-	int x;
-	int y;
-	public:
-	int getx();
-	int gety();
-	void setx(int x1);
-	void sety(int y1);
-};
-
-class Board{
-
-public:
-	/**
-     * Constructor : Creates the empty board with default size corresponding to 5 rings
-     * 
-     */
-	Board();
-
-	/**
-     * Constructor : Creates the empty board with default size corresponding to 5 rings
-     * 
-     */
-
-	Board( int boardsize);
-
-	Point twod_from_hex (Point p);
-	
-	Point hex_from_twod (Point p);
-
-	/**
-     * to set the position of a ring on the board.
-     * 
-     */
-	/**
-     * -1 :  empty , 0 : whitering , 1 :blackring , 2 : whitemarker , 3 : blackmarker  
-     * 
-     */
-
-
-	void setRing(int ringcolor,int px, int py);// ringid , hexagon , position
-
-	/**
-     * to move the position of a ring on the board, and set the board accordingly.
-     * 
-     */
-	void moveRing(int ringcolor, int from_x,int from_y, int to_x,int to_y);
-
-	/**
-     * to remove a ring from the board, and set the board accordingly.
-     * 
-     */
-	void removeRing(int opponentID ,int from_x ,int from_y ,int  to_x ,int  to_y ,int ring_removeX ,int ring_removeY);
-	// from_x , from_y to to_x to_y ==remove markers here of colour (opponentID+2).
-	// remove ring from ring_removeX , ring_removeY
-	/**
-     * to calculate the evaluation function.
-     * 
-     */
-	float evaluate();
-
-	void getNeighbours(int px,int py, int ringcolor);
-
-
-
-};
-
-
-
-#endif	/* BOARD_H */
-
-
->>>>>>> d3567809e16911af80bd3c4d6703a6e5e650c95c
